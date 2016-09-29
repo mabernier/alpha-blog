a047ba3ef9250f016cf000e631c244afcfff77ad
mabernier-alpha-blog <mabernier@saskgraphics.com>
2016-09-29 16:22:22 +0000

    Add one to many association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article< ActiveRecord::Base
+    belongs_to :user
     validates :title, presence: true, length: {minimum:5, maximum: 100}
     validates :description, presence: true, length: {minimum:10, maximum: 300}
-
+    validates :user_id, presence: true
 end
