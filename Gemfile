144d563384d3cd4fe260768869868c118dc1358e
mabernier-alpha-blog <mabernier@saskgraphics.com>
2016-10-09 19:26:23 +0000

    Add secure pwd ant attribute to users

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ ruby '2.2.4'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
-# Use sqlite3 as the database for Active Record
+gem 'bcrypt', '~> 3.1.7'
 
 # Use SCSS for stylesheets
 gem 'bootstrap-sass', '~> 3.3.6'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: {case_sensitive: false}, length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 125}, uniqueness: {case_sensitive: false}, format: { with: VALID_EMAIL_REGEX } 
+  validates :email, presence: true, length: {maximum: 125}, uniqueness: {case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
